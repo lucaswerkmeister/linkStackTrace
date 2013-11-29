f0285677af9d54b3c8e647dc8de9ee5aff2d43d7
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-29 11:21:12 +0100

    Create config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Make changes here for your configuration then rename this file to config.php.
+ */
+
+define("CLIENT_ID", "put your applications’s client ID here");
+define("CLIENT_SECRET", "put your application’s client secret here");
+define("STATE", "an unguessable random string");

0cc290dd300531ebbe509365e88db07d8f655317
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-29 20:58:45 +0100

    Ditch PHP oauth, use hard-coded token
    That token has no scopes, so I don’t think anyone can do anything
    harmful to my account with it

--- config-sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Make changes here for your configuration then rename this file to config.php.
- */
-
-define("CLIENT_ID", "put your applications’s client ID here");
-define("CLIENT_SECRET", "put your application’s client secret here");
-define("STATE", "an unguessable random string");
